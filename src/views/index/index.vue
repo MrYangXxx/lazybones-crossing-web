13fe70d6c59ff0d9fcfc755ff6c16690d173e7ed
yx <yx@qq.com>
2020-09-14 09:47:55 +0800

    首页

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,16 +1,45 @@
 <template>
-  <div class="dashboard-text" style="top: 40%;position: absolute;left: 50%;-webkit-transform: translateX(-50%);">欢迎来到：我趣网后台管理系统</div>
+  <ul v-infinite-scroll="load" class="infinite-list" style="overflow: auto;" infinite-scroll-distance="0">
+    <li v-for="record in recordList" class="infinite-list-item" style="list-style-type:none;padding-bottom: 20px">
+      <el-card style="height: 150px;width: 80%">
+        <div>
+          {{ record.content }}
+        </div>
+      </el-card>
+    </li>
+  </ul>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import { listRecords } from '@/api/record'
 
 export default {
-  name: 'Dashboard',
+  name: 'Index',
+  data() {
+    return {
+      count: 10,
+      recordList: []
+    }
+  },
   computed: {
     ...mapGetters([
       'name'
     ])
+  },
+  created() {
+    this.getRecords()
+  },
+  methods: {
+    load() {
+      if (this.count < 30) {
+        this.count += 2
+      }
+    },
+    async getRecords() {
+      const res = await listRecords({})
+      this.recordList = res.data.records
+    }
   }
 }
 </script>
