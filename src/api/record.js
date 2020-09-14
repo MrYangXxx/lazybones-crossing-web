eba6a7e3288206081a099f0ffbea5b0133afcdce
yx <yx@qq.com>
2020-09-14 09:49:30 +0800

    增加查询api

--- src/api/record.js
+++ src/api/record.js
@@ -2,7 +2,23 @@ import request from '@/utils/request'
 
 export function addRecord(data) {
   return request({
-    url: '/record/add',
+    url: '/record',
+    method: 'post',
+    data
+  })
+}
+
+export function listRecords(data) {
+  return request({
+    url: '/record/find',
+    method: 'post',
+    data
+  })
+}
+
+export function myRecords(data) {
+  return request({
+    url: '/record/user',
     method: 'post',
     data
   })
